934c10c6850d6a1004744c497a51d2ef8c17e61b
Sam0fc <sammyhutton@gmail.com>
2024-10-19 15:20:05 +0100

    added segnet

--- Pets.py
+++ Pets.py
@@ -138,7 +138,7 @@ class CustomImageDataset(TD.Dataset):
             target = self.post_target_transform(target)
 
 
-        return (image,target)
+        return (image.float()/256,target.float())
 
 
 #define some transforms:
@@ -190,6 +190,145 @@ t2img(trimap2f(torchvision.utils.make_grid(targets,nrow=8)))
 
 #split data
 train_data, test_data = TD.random_split(full_dataset,[0.7,0.3])
+
+pets_train_loader = TD.DataLoader(
+    train_data,
+    batch_size=32,
+    shuffle=True,
+)
+
+pets_test_loader = TD.DataLoader(
+    test_data,
+    batch_size=21,
+    shuffle=True,
+)
+
+train_pets_inputs, train_pets_targets = next(iter(pets_train_loader))
+test_pets_inputs, test_pets_targets = next(iter(pets_test_loader))
+train_pets_inputs.shape, train_pets_targets.shape
+
+## THE ACTUAL MODEL!
+class DownConv2(nn.Module):
+    def __init__(self, chin, chout, kernel_size):
+        super().__init__()
+        self.seq = nn.Sequential(
+                nn.Conv2d(in_channels=chin, out_channels=chout, kernel_size=kernel_size, padding=kernel_size//2, bias=False),
+                nn.BatchNorm2d(chout),
+                nn.ReLU(),
+                nn.Conv2d(in_channels=chout, out_channels=chout, kernel_size=kernel_size, padding=kernel_size//2, bias=False),
+                nn.BatchNorm2d(chout),
+                nn.ReLU(),
+        )
+        self.mp=nn.MaxPool2d(kernel_size=2, return_indices=True)
+
+    def forward(self,x):
+        y = self.seq(x)
+        pool_shape = y.shape
+        y, indices = self.mp(y)
+        return y, indices, pool_shape
+
+class DownConv3(nn.Module):
+    def __init__(self, chin, chout, kernel_size):
+        super().__init__()
+        self.seq = nn.Sequential(
+                nn.Conv2d(in_channels=chin, out_channels=chout, kernel_size=kernel_size, padding=kernel_size//2, bias=False),
+                nn.BatchNorm2d(chout),
+                nn.ReLU(),
+                nn.Conv2d(in_channels=chout, out_channels=chout, kernel_size=kernel_size, padding=kernel_size//2, bias=False),
+                nn.BatchNorm2d(chout),
+                nn.ReLU(),
+                nn.Conv2d(in_channels=chout, out_channels=chout, kernel_size=kernel_size, padding=kernel_size//2, bias=False),
+                nn.BatchNorm2d(chout),
+                nn.ReLU(),
+        )
+        self.mp = nn.MaxPool2d(kernel_size=2, return_indices=True)
+
+    def forward(self, x):
+        y = self.seq(x)
+        pool_shape = y.shape
+        y, indices = self.mp(y)
+        return y, indices, pool_shape
+
+
+class UpConv2(nn.Module):
+    def __init__(self, chin, chout, kernel_size):
+        super().__init__()
+        self.seq = nn.Sequential(
+                nn.Conv2d(in_channels=chin, out_channels=chin, kernel_size=kernel_size, padding=kernel_size//2, bias=False),
+                nn.BatchNorm2d(chin),
+                nn.ReLU(),
+                nn.Conv2d(in_channels=chin, out_channels=chout, kernel_size=kernel_size, padding=kernel_size//2, bias=False),
+                nn.BatchNorm2d(chout),
+                nn.ReLU(),
+        )
+        self.mup = nn.MaxUnpool2d(kernel_size=2)
+
+    def forward(self, x, indices, output_size):
+        y = self.mup(x, indices, output_size=output_size)
+        y = self.seq(y)
+        return y
+
+class UpConv3(nn.Module):
+    def __init__(self, chin, chout, kernel_size):
+        super().__init__()
+        self.seq = nn.Sequential(
+                nn.Conv2d(in_channels=chin, out_channels=chin, kernel_size=kernel_size, padding=kernel_size//2, bias=False),
+                nn.BatchNorm2d(chin),
+                nn.ReLU(),
+                nn.Conv2d(in_channels=chin, out_channels=chin, kernel_size=kernel_size, padding=kernel_size//2, bias=False),
+                nn.BatchNorm2d(chin),
+                nn.ReLU(),
+                nn.Conv2d(in_channels=chin, out_channels=chout, kernel_size=kernel_size, padding=kernel_size//2, bias=False),
+                nn.BatchNorm2d(chout),
+                nn.ReLU(),
+        )
+        self.mup = nn.MaxUnpool2d(kernel_size=2)
+    
+    def forward(self, x, indices, output_size):
+        y = self.mup(x, indices, output_size=output_size)
+        y = self.seq(y)
+        return y
+
+class ImageSegmentation(torch.nn.Module):
+    def __init__(self, kernel_size):
+        super().__init__()
+        self.out_channels = 3 
+        self.bn_input = nn.BatchNorm2d(3)
+        
+        self.dc1 = DownConv2(3,64, kernel_size=kernel_size)
+        self.dc2 = DownConv2(64,128, kernel_size=kernel_size)
+        self.dc3 = DownConv3(128,256, kernel_size=kernel_size)
+        self.dc4 = DownConv3(256,512, kernel_size=kernel_size)
+
+        self.uc4 = UpConv3(512,256, kernel_size=kernel_size)
+        self.uc3 = UpConv3(256,128, kernel_size=kernel_size)
+        self.uc2 = UpConv2(128,64, kernel_size=kernel_size)
+        self.uc1 = UpConv2(64,3, kernel_size=kernel_size)
+
+    def forward(self, batch: torch.Tensor):
+        x = self.bn_input(batch)
+
+        x, mp1_indices, shape1 = self.dc1(x)
+        x, mp2_indices, shape2 = self.dc2(x)
+        x, mp3_indices, shape3 = self.dc3(x)
+        x, mp4_indices, shape4 = self.dc4(x)
+
+        x = self.uc4(x, mp4_indices, output_size=shape4)
+        x = self.uc3(x, mp3_indices, output_size=shape3)
+        x = self.uc2(x, mp2_indices, output_size=shape2)
+        x = self.uc1(x, mp1_indices, output_size=shape1)
+
+        return x
+
+# DONE
+
+torch.cuda.empty_cache()
+
+#single batch checker
+m = ImageSegmentation(kernel_size=3)
+m.eval()
+to_device(m)
+m(to_device(train_pets_inputs)).shape
 
 
 
